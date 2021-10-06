937558f033f0289e355604bb9c91de874c7c8aca
jchoi11 <jchoi11@SWEN1017-06>
2021-10-06 10:57:01 -0500

    Add project files.

---
+++ testLab2/Graph.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testLab2
+{
+    class Graph
+    {
+        private object[] vertices;
+        private bool[,] edges;
+
+        public Graph(int numVertices)
+        {
+            vertices = new object[numVertices];
+            edges = new bool[numVertices, numVertices];
+        }
+        public void addVertexData(int vertexNumber, object vertexData)
+        {
+            // TO DO: throw an exception if vertexNumber is bad
+            vertices[vertexNumber] = vertexData;
+        }
+        public void addEdge(int vertex1, int vertex2)
+        {
+        }
+        public void removeEdge(int vertex1, int vertex2)
+        {
+        }
+        public bool hasEdge(int vertex1, int vertex2)
+        {
+            return false;
+        }
+        public object getVertexData(int vertexNumber)
+        {
+            // TO DO: throw an exception if vertexNumber is bad
+            return vertices[vertexNumber];
+        }
+        public int getNumVertices()
+        {
+            return 0;
+        }
+        public bool isConnected()
+        {
+            return false;
+        }
+        public bool hasCycle()
+        {
+            return false;
+        }
+        public bool isTree()
+        {
+            return false;
+        }
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
---
+++ testLab2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace testLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            1.Write C# code that does each of the following tasks. For parts (d), (e), and (f), also find the expected result and explain how you got it.
+
+            (a) creates a Graph g with 128 vertices;*/
+            // CALL graph's int constructor.
+            // syntax is same as for Java:
+            int graphSize = 128;
+            Graph g = new Graph(graphSize);
+
+            
+            // (b)stores the numbers 0 through 127 in the vertices, in that order;
+            for(int i = 0; i< graphSize; i++)
+            {
+                g.addVertexData(i, g.getVertexData(i));
+            }
+
+           
+
+            /*
+            
+
+            (c)joins all pairs of vertices of the form { j, j / 2}
+            where j is between 1 and 127, and where j / 2 is computed as usual with integer division;
+
+            (d)tests g for connectedness;
+            expected results:
+
+
+
+            (e)tests g for cycles;
+            expected results:
+
+
+
+            (f)tests whether g is a tree.
+
+            expected results:
+*/
+
+
+
+
+
+
+                         Console.WriteLine("Hello World!");
+        }
+    }
+}
